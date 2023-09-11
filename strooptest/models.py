a9fbc992d633943b1c4a120c6ea05f92f773d979
sirojiddin4 <boboqulovsirojiddin4@gmail.com>
2023-09-11 10:42:33 +0500

    initial commit

--- strooptest/models.py
+++ strooptest/models.py
@@ -2,57 +2,56 @@ from django.db import models
 
 class Questionnaire(models.Model):
     # Consent
-    consent = models.BooleanField(default=False, verbose_name="I have read the above information and consent to participate in this study.")
-    
+    consent = models.BooleanField(default=False, verbose_name="I have read the above information and consent to participate in this study. (Men yuqoridagi ma'lumotni o'qib chiqdim va bu tadqiqotda ishtirok etishga rozimanman.)")
+
     # Personal Details
-    age = models.IntegerField()
+    age = models.IntegerField(verbose_name="Age (Yoshingiz nechida?)")
     GENDER_CHOICES = [
-        ('M', 'Male'),
-        ('F', 'Female'),
-        ('NB', 'Non-Binary'),
-        ('NS', 'Prefer not to say'),
+        ('M', 'Male (Erkak)'),
+        ('F', 'Female (Ayol)'),
+        ('NB', 'Non-Binary (Noaniq)'),
+        ('NS', 'Prefer not to say (Aytishni xohlamayman)'),
     ]
-    gender = models.CharField(max_length=2, choices=GENDER_CHOICES)
+    gender = models.CharField(max_length=2, choices=GENDER_CHOICES, verbose_name="Gender (Jinsingiz)")
     EDUCATION_CHOICES = [
-        ('HS', 'High School or less'),
-        ('SC', 'Some College'),
-        ('BD', 'Bachelor\'s Degree'),
-        ('MD', 'Master\'s Degree'),
-        ('PHD', 'PhD or higher'),
+        ('HS', 'High School or less (Oliy maktab yoki undan pastroq)'),
+        ('SC', 'Some College (Bazi kollej)'),
+        ('BD', 'Bachelor\'s Degree (Bakalavr darajasi)'),
+        ('MD', 'Master\'s Degree (Magistr darajasi)'),
+        ('PHD', 'PhD or higher (PhD yoki undan yuqori)'),
     ]
-    education = models.CharField(max_length=3, choices=EDUCATION_CHOICES)
+    education = models.CharField(max_length=3, choices=EDUCATION_CHOICES, verbose_name="Education Level (Ma'lumotingiz)")
 
     # Potential Confounding Variables
-    sleep_hours = models.IntegerField(choices=[(i, str(i)) for i in range(0, 25)], verbose_name="How many hours of sleep did you get last night?")
-    caffeine_consumed = models.BooleanField(verbose_name="Have you consumed any caffeinated beverages today?")
-    alcohol_consumed = models.BooleanField(verbose_name="Have you consumed alcohol in the last 24 hours?")
-    medication = models.BooleanField(verbose_name="Are you currently taking any medication that affects attention or cognitive function?")
-    medication_details = models.TextField(blank=True, verbose_name="If yes, please specify:")
+    sleep_hours = models.IntegerField(choices=[(i, str(i)) for i in range(0, 25)], verbose_name="How many hours of sleep did you get last night? (Siz kechqurun nechta soat uxladingiz?)")
+    caffeine_consumed = models.BooleanField(verbose_name="Have you consumed any caffeinated beverages today? (Siz bugun kofeinli ichimlik iste'mol qildingizmi?)")
+    alcohol_consumed = models.BooleanField(verbose_name="Have you consumed alcohol in the last 24 hours? (Siz oxirgi 24 soatda alkogol iste'mol qildingizmi?)")
+    medication = models.BooleanField(verbose_name="Are you currently taking any medication that affects attention or cognitive function? (Siz hozirda diqqat yoki kognitiv funksiyani ta'sir qiladigan dori vositalarini qabul qilyapsizmi?)")
+    medication_details = models.TextField(blank=True, verbose_name="If yes, please specify: (Agar ha, iltimos ko'rsating:)")
     video_watch_frequency = models.CharField(
         max_length=2,
-        choices=[
-            ('R', 'Rarely'),
-            ('S', 'Sometimes'),
-            ('F', 'Frequently'),
-            ('V', 'Very frequently'),
-        ],
-        verbose_name="How often do you watch short videos (less than 1 minute) on platforms like TikTok, Instagram, etc.?"
+        choices=[ ('R', 'Rarely (Kamchilik)'),
+            ('S', 'Sometimes (Ba\'zan)'),
+            ('F', 'Frequently (Tez-tez)'),
+            ('V', 'Very frequently (Juda ko\'p)') ],
+        verbose_name="How often do you watch short videos (less than 1 minute) on platforms like TikTok, Instagram, etc.? (Siz TikTok, Instagram kabi platformalarda (1 daqiqadan kamroq) qisqa videolarni qanchalik tez-tez ko'rasiz?)"
     )
-    stroop_experience = models.BooleanField(verbose_name="Have you ever participated in a Stroop Test before?")
+    stroop_experience = models.BooleanField(verbose_name="Have you ever participated in a Stroop Test before? (Siz avvalgi Stroop Testida qatnashganmisiz?)")
     current_mood = models.CharField(
         max_length=2,
         choices=[
-            ('VN', 'Very Negative'),
-            ('N', 'Negative'),
-            ('NE', 'Neutral'),
-            ('P', 'Positive'),
-            ('VP', 'Very Positive'),
+            ('VN', 'Very Negative (Juda salbiy)'),
+            ('N', 'Negative (Salbiy)'),
+            ('NE', 'Neutral (Noaniq)'),
+            ('P', 'Positive (Ijobiy)'),
+            ('VP', 'Very Positive (Juda ijobiy)'),
         ],
-        verbose_name="How would you rate your current mood?"
+        verbose_name="How would you rate your current mood? (Qanday darajada joriy kayfiyatingizga baho berishingiz mumkin?)"
     )
 
     # Other Information
-    email = models.EmailField()
+    email = models.EmailField(verbose_name="Email (Elektron pochta )", unique=True)
+
 
     def __str__(self):
         return self.email
@@ -64,3 +63,8 @@ class StroopTest(models.Model):
     is_correct = models.BooleanField()
     word_color_pair = models.CharField(max_length=50)  # New field
 
+class UserStats(models.Model):
+    user = models.ForeignKey(Questionnaire, on_delete=models.CASCADE)
+    phase = models.CharField(max_length=10)
+    avg_accuracy = models.FloatField()
+    avg_response_time = models.FloatField()
--- strooptest/views.py
+++ strooptest/views.py
@@ -1,55 +1,78 @@
-# views.py
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .forms import QuestionnaireForm, StroopTestForm
-from .models import Questionnaire, StroopTest
+from .models import Questionnaire, StroopTest, UserStats
 import random
 from datetime import datetime
+from django.db import IntegrityError
 
 def questionnaire_view(request):
     form = QuestionnaireForm()
+    error_message = None
+    
     if request.method == 'POST':
         form = QuestionnaireForm(request.POST)
         if form.is_valid():
-            questionnaire = form.save()
-            request.session['user_id'] = questionnaire.id  # Store user ID in session
-            return redirect('instructions_phase1')  # Redirect to the instructions page for Phase 1
-    return render(request, 'stroop/questionnaire.html', {'form': form})
+            try:
+                
+                questionnaire = form.save()
+                request.session['user_id'] = questionnaire.id
+                request.session['user_email'] = questionnaire.email  # store email in session
+                return redirect('instructions_phase1')
+            except IntegrityError:
+                error_message = "This email has already been used. Please use a different email."
 
-def success(request):
-    return render(request, 'stroop/success.html')
+    return render(request, 'stroop/questionnaire.html', {'form': form, 'error_message': error_message})
+
+
 
 def instructions_phase1(request):
     return render(request, 'stroop/stroop_instructions_phase_1.html')
 
-
 def stroop_test_phase1_view(request):
+    
+    user_id = request.session.get('user_id')
+    if not user_id:
+        return redirect('questionnaire')
+    user = Questionnaire.objects.get(id=user_id)
+
     if 'question_count' not in request.session:
         request.session['question_count'] = 0
-    
+
     if request.method == "POST":
+        selected_color = request.POST.get("response")
+        if request.session['task'] == 'C':
+            is_correct = (selected_color.upper() == request.session['display_color'])
+        else:
+            is_correct = (selected_color.upper() == request.session['text'])
+
         request.session['question_count'] += 1
         if request.session['question_count'] >= 10:
-            # Reset for the next phases
             request.session['question_count'] = 0
             return redirect('instructions_phase2')
 
     colors = ['RED', 'BLUE', 'GREEN', 'YELLOW', 'ORANGE', 'PURPLE', 'BLACK']
     text = random.choice(colors)
     display_color = random.choice(colors)
-    
-    return render(request, 'stroop/stroop_test_phase1.html', {'text': text, 'display_color': display_color})
+    task = random.choice(['C', 'W', 'C', 'W'])
+
+    request.session['text'] = text
+    request.session['display_color'] = display_color
+    request.session['task'] = task
+
+ 
+
+    return render(request, 'stroop/stroop_test_phase1.html', {'text': text, 'display_color': display_color, 'task': task})
 
 def instructions_phase2(request):
     return render(request, 'stroop/stroop_instructions_phase_2.html')
 
 def stroop_test_phase2_view(request):
-    # Retrieve user ID from session
+    
     user_id = request.session.get('user_id')
     if not user_id:
-        return redirect('questionnaire')  # Redirect to the questionnaire if user_id is not found
+        return redirect('questionnaire')
 
-    # Retrieve the Questionnaire object using the user ID
     user = Questionnaire.objects.get(id=user_id)
 
     if 'question_count' not in request.session:
@@ -59,46 +82,52 @@ def stroop_test_phase2_view(request):
         request.session['start_time'] = datetime.now().timestamp()
 
     if request.method == "POST":
+        
         elapsed_time = datetime.now().timestamp() - request.session['start_time']
         selected_color = request.POST.get("response")
-        is_correct = (selected_color.upper() == request.session['text'])
+        if request.session['task'] == 'C':
+            is_correct = (selected_color.upper() == request.session['display_color'])
+        elif request.session['task'] == 'W':
+            is_correct = (selected_color.upper() == request.session['text'])
 
-        # Record details
         stroop_test = StroopTest(
-            user=user,  # Use the user fetched from the session
+            user=user,
             test_type='Phase 2',
             response_time=elapsed_time,
             is_correct=is_correct,
-            word_color_pair=f"{request.session['text']}_{request.session['display_color']}"
+            word_color_pair=f"{request.session.get('text')}_{request.session.get('display_color')}"
         )
         stroop_test.save()
 
         request.session['question_count'] += 1
 
-        if request.session['question_count'] >= 35:
+        if request.session['question_count'] >= 50:
             request.session['question_count'] = 0
             return redirect('instructions_phase3')
-
+    
+    # Generate new values AFTER processing the POST request.
     colors = ['RED', 'BLUE', 'GREEN', 'YELLOW', 'ORANGE', 'PURPLE', 'BLACK']
     text = random.choice(colors)
     display_color = random.choice(colors)
+    task = random.choice(['C', 'W'])
 
     request.session['text'] = text
     request.session['display_color'] = display_color
+    request.session['task'] = task
+    # Example for phase 1
 
-    return render(request, 'stroop/stroop_test_phase2.html', {'text': text, 'display_color': display_color})
+
+    return render(request, 'stroop/stroop_test_phase2.html', {'text': text, 'display_color': display_color, 'task': task})
 
 def instructions_phase3(request):
     return render(request, 'stroop/stroop_instructions_phase_3.html')
 
-
 def stroop_test_phase3_view(request):
-    # Retrieve user ID from session
+
     user_id = request.session.get('user_id')
     if not user_id:
-        return redirect('questionnaire')  # Redirect to the questionnaire if user_id is not found
+        return redirect('questionnaire')
 
-    # Retrieve the Questionnaire object using the user ID
     user = Questionnaire.objects.get(id=user_id)
 
     if 'question_count' not in request.session:
@@ -110,11 +139,13 @@ def stroop_test_phase3_view(request):
     if request.method == "POST":
         elapsed_time = datetime.now().timestamp() - request.session['start_time']
         selected_color = request.POST.get("response")
-        is_correct = (selected_color.upper() == request.session['text'])
+        if request.session['task'] == 'C':
+            is_correct = (selected_color.upper() == request.session['display_color'])
+        else:
+            is_correct = (selected_color.upper() == request.session['text'])
 
-        # Record details
         stroop_test = StroopTest(
-            user=user,  # Use the user fetched from the session
+            user=user,
             test_type='Phase 3',
             response_time=elapsed_time,
             is_correct=is_correct,
@@ -124,10 +155,11 @@ def stroop_test_phase3_view(request):
 
         request.session['question_count'] += 1
 
-        if request.session['question_count'] >= 35:
+        if request.session['question_count'] >= 50:
             request.session['question_count'] = 0
-            return redirect('success')  # Redirect to the success page
+            return redirect('success')
 
+    # Generate new values AFTER processing the POST request.
     colors = ['RED', 'BLUE', 'GREEN', 'YELLOW', 'ORANGE', 'PURPLE', 'BLACK']
     text = random.choice(colors)
     display_color = random.choice(colors)
@@ -135,4 +167,11 @@ def stroop_test_phase3_view(request):
     request.session['text'] = text
     request.session['display_color'] = display_color
 
-    return render(request, 'stroop/stroop_test_phase3.html', {'text': text, 'display_color': display_color})
+    task = random.choice(['C', 'W', 'C', 'W'])
+    request.session['task'] = task
+
+
+    return render(request, 'stroop/stroop_test_phase3.html', {'text': text, 'display_color': display_color, 'task': task})
+
+def success(request):
+    return render(request, 'stroop/success.html')
